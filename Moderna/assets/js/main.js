9e3b6ed2fe1acca7bf7a64f4d5a0d3cecaeddd13
yuchansun <nicosun0515@gmail.com>
2025-04-16 00:09:58 +0800

    footer and index

--- Moderna/assets/js/main.js
+++ Moderna/assets/js/main.js
@@ -105,15 +105,15 @@
   /**
    * Auto generate the carousel indicators
    */
-  document.querySelectorAll('.carousel-indicators').forEach((carouselIndicator) => {
-    carouselIndicator.closest('.carousel').querySelectorAll('.carousel-item').forEach((carouselItem, index) => {
-      if (index === 0) {
-        carouselIndicator.innerHTML += `<li data-bs-target="#${carouselIndicator.closest('.carousel').id}" data-bs-slide-to="${index}" class="active"></li>`;
-      } else {
-        carouselIndicator.innerHTML += `<li data-bs-target="#${carouselIndicator.closest('.carousel').id}" data-bs-slide-to="${index}"></li>`;
-      }
-    });
-  });
+  // document.querySelectorAll('.carousel-indicators').forEach((carouselIndicator) => {
+  //   carouselIndicator.closest('.carousel').querySelectorAll('.carousel-item').forEach((carouselItem, index) => {
+  //     if (index === 0) {
+  //       carouselIndicator.innerHTML += `<li data-bs-target="#${carouselIndicator.closest('.carousel').id}" data-bs-slide-to="${index}" class="active"></li>`;
+  //     } else {
+  //       carouselIndicator.innerHTML += `<li data-bs-target="#${carouselIndicator.closest('.carousel').id}" data-bs-slide-to="${index}"></li>`;
+  //     }
+  //   });
+  // });
 
   /**
    * Initiate glightbox
