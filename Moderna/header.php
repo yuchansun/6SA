2d0bfcb58b10a9546660487dcda6b8bfb50ee3fc
julie0913-ai <julie70212@gmail.com>
2025-05-07 01:46:09 +0800

    管理者header

--- Moderna/header.php
+++ Moderna/header.php
@@ -81,42 +81,71 @@ if ($currentPage === 'blog-details.php' && !isset($_SESSION['user'])) {
 
     <nav id="navmenu" class="navmenu">
       <ul>
-        <li><a href="index.php">首頁</a></li>
-        <li><a href="about.php">校系簡章</a></li>
-        <li><a href="favorite.php">我的最愛</a></li>
-        <li><a href="blog-details.php">討論區</a></li>
-        <li class="dropdown"><a href="#"><span>學群及學校介紹</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-          <ul>
-            <li><a href="services.php">學群介紹</a></li>
-            <li><a href="portfolio.php">學校簡介</a></li>
-          </ul>
-        </li>
-
-        <?php if (isset($_SESSION['nickname'])): ?> 
-          <li>
-            <a href="javascript:void(0);" id="openNotifications">
-              <i class="bi bi-bell-fill"></i>
-            </a>
+        <!-- 未登入時顯示基本選項 -->
+        <?php if (!isset($_SESSION['nickname'])): ?> 
+          <li><a href="index.php">首頁</a></li>
+          <li><a href="about.php">校系簡章</a></li>
+          <li><a href="favorite.php">我的最愛</a></li>
+          <li><a href="blog-details.php">討論區</a></li>
+          <li class="dropdown">
+            <a href="#"><span>學群及學校介紹</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="services.php">學群介紹</a></li>
+              <li><a href="portfolio.php">學校簡介</a></li>
+            </ul>
           </li>
-
-          <li class="d-flex align-items-center"><a href="update_personal.php">
-            <img src="<?= htmlspecialchars($photoPath) ?>" alt="Profile" style="width: 30px; height: 30px; border-radius: 50%; object-fit: cover; margin-right: 6px;">
-            <span>Hi, <?= htmlspecialchars($_SESSION['nickname']) ?></span></a>
-          </li>
-          <li><a href="logout.php">登出</a></li>
-
-          <?php if ($userRole === '管理者'): ?>
-            <li><a href="admin.php">管理者操作</a></li>
-          <?php endif; ?>
-        <?php else: ?>
           <li><a href="contact.php">登入</a></li>
         <?php endif; ?>
+
+        <!-- 登入後顯示用戶相關選項 -->
+        <?php if (isset($_SESSION['nickname'])): ?> 
+          <?php if ($userRole !== '管理者'): ?>
+            <!-- 普通使用者看到的選項 -->
+            <li><a href="index.php">首頁</a></li>
+            <li><a href="about.php">校系簡章</a></li>
+            <li><a href="favorite.php">我的最愛</a></li>
+            <li><a href="blog-details.php">討論區</a></li>
+            <li class="dropdown">
+              <a href="#"><span>學群及學校介紹</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+              <ul>
+                <li><a href="services.php">學群介紹</a></li>
+                <li><a href="portfolio.php">學校簡介</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="javascript:void(0);" id="openNotifications">
+                <i class="bi bi-bell-fill"></i>
+              </a>
+            </li> 
+            <li class="d-flex align-items-center"><a href="update_personal.php">
+              <img src="<?= htmlspecialchars($photoPath) ?>" alt="Profile" style="width: 30px; height: 30px; border-radius: 50%; object-fit: cover; margin-right: 6px;">
+              <span>Hi, <?= htmlspecialchars($_SESSION['nickname']) ?></span></a>
+            </li>
+            <!-- 登出選項 -->
+            <li><a href="logout.php">登出</a></li>
+          <?php endif; ?>
+
+          <!-- 管理者特有選項 -->
+          <?php if ($userRole === '管理者'): ?>
+            <li><a href="">管理校系簡章</a></li>
+            <li><a href="blog-details.php">管理討論區</a></li>
+            <li><a href="teacher_verify.php">教師驗證</a></li>
+            <li><a href="">新增管理者</a></li>
+            <li class="d-flex align-items-center"><a href="update_personal.php">
+              <img src="<?= htmlspecialchars($photoPath) ?>" alt="Profile" style="width: 30px; height: 30px; border-radius: 50%; object-fit: cover; margin-right: 6px;">
+              <span>Hi, <?= htmlspecialchars($_SESSION['nickname']) ?></span></a>
+            </li>
+            <li><a href="logout.php">登出</a></li>
+          <?php endif; ?>
+        <?php endif; ?>
+
       </ul>
       <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
     </nav>
 
   </div>
 </header>
+
 
 <!-- 通知側欄 -->
 <div id="notificationSidebar" class="notification-sidebar">
