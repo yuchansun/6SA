8f3ad8175802b7d6e4c995203c19008c179680fa
ding <ttlion08@gmail.com>
2025-05-14 20:37:54 +0800

    新增必填報錯訊息改善

--- Moderna/add_school.php
+++ Moderna/add_school.php
@@ -316,7 +316,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     alert(data.message);
                     window.location.href = data.redirect;
                 } else {
-                    alert('新增失敗：' + data.message);
+                    alert(data.message);
                 }
             })
             .catch(error => {
